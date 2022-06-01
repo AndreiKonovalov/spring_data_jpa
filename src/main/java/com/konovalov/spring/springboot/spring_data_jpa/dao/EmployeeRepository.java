620cc1aa8d74cab64411771177491d4ae4279477
AndreiKonovalov <96517761+AndreiKonovalov@users.noreply.github.com>
2022-06-01 12:55:17 +0500

    Done

--- src/main/java/com/konovalov/spring/springboot/spring_data_jpa/dao/EmployeeRepository.java
+++ src/main/java/com/konovalov/spring/springboot/spring_data_jpa/dao/EmployeeRepository.java
@@ -3,5 +3,9 @@ package com.konovalov.spring.springboot.spring_data_jpa.dao;
 import com.konovalov.spring.springboot.spring_data_jpa.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    public List<Employee> findAllByName(String name);
 }
