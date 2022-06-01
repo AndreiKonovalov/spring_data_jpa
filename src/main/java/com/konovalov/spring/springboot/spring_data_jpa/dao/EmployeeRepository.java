0e77b9c68a03741d7c68d93b8d3ced8a9d501ccc
AndreiKonovalov <96517761+AndreiKonovalov@users.noreply.github.com>
2022-06-01 12:16:41 +0500

    Initial commit

---
+++ src/main/java/com/konovalov/spring/springboot/spring_data_jpa/dao/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package com.konovalov.spring.springboot.spring_data_jpa.dao;
+
+import com.konovalov.spring.springboot.spring_data_jpa.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+}
